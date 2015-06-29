ab65f82823f5843f592932b072c1f64c4c5def85
Angelko Krstanovic <angelko4fun@gmail.com>
2015-06-28 17:11:18 -0700

    Display artist images

--- app/src/main/java/com/example/r626926/spotify_streamer/ArtistListAdapter.java
+++ app/src/main/java/com/example/r626926/spotify_streamer/ArtistListAdapter.java
@@ -5,7 +5,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -31,6 +34,12 @@ public class ArtistListAdapter extends ArrayAdapter<Artist> {
         }
         TextView artistName = (TextView) convertView.findViewById(R.id.listview_artist_name);
         artistName.setText(artist.name);
+
+        ImageView iconView = (ImageView) convertView.findViewById(R.id.list_item_artist_image);
+        if (artist.images.size() > 0) {
+            int imageIndex = artist.images.size() - 1;
+            Picasso.with(parent.getContext()).load(artist.images.get(imageIndex).url.toString()).into(iconView);
+        }
 
         return convertView;
     }
