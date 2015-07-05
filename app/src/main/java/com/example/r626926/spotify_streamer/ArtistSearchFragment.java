787633b6252e3543018c61c2359d0226d1c9e70c
Angelko Krstanovic <angelko4fun@gmail.com>
2015-07-04 22:48:58 -0700

    Setup artist list lick listener
    http://developer.android.com/reference/android/widget/AdapterView.OnItemClickListener.html

--- app/src/main/java/com/example/r626926/spotify_streamer/ArtistSearchFragment.java
+++ app/src/main/java/com/example/r626926/spotify_streamer/ArtistSearchFragment.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SearchView;
 import android.widget.Toast;
@@ -76,6 +77,13 @@ public class ArtistSearchFragment extends Fragment {
                     }
                 }
         );
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.d(TAG, "click");
+            }
+        });
         return rootView;
     }
 
